d8f3d88f0134d15ae11b1a6ed1cb95e076c01985
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-02-24 12:33:57 +0600

    solving cors error

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://bike-solution-rakibul.vercel.app/",
+    origin: "https://bike-solution-rakibul.vercel.app",
     credentials: true,
   })
 );
