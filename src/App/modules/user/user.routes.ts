3cdc44386aa31e0c34fa77c1aea999492073f3cf
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-02-24 12:47:32 +0600

    grant auth permission to admin

--- src/App/modules/user/user.routes.ts
+++ src/App/modules/user/user.routes.ts
@@ -1,33 +1,21 @@
-import express from 'express';
-import { userControllers } from './user.controller';
-import validateRequest from '../../middlewares/validateRequest';
-import userValidationSchema from './user.validation';
-import auth from '../../middlewares/auth';
+import express from "express";
+import { userControllers } from "./user.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import userValidationSchema from "./user.validation";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
 router.post(
-  '/register-user',
+  "/register-user",
   validateRequest(userValidationSchema),
-  userControllers.createUser,
+  userControllers.createUser
 );
 
-router.get(
-  '/',
-  auth('admin'),
-  userControllers.getAllUsers,
-)
+router.get("/", auth("admin"), userControllers.getAllUsers);
 
-router.get(
-  '/:email',
-  auth('admin', 'customer'),
-  userControllers.getSingleUser,
-)
+router.get("/:email", auth("admin", "customer"), userControllers.getSingleUser);
 
-router.patch(
-  '/:userId',
-  auth('customer'),
-  userControllers.updateUser,
-)
+router.patch("/:userId", auth("customer", "admin"), userControllers.updateUser);
 
 export const userRoutes = router;
