1c785f58d252995cee8ecc1dc293d8041ae42d64
Rakibul-98 <rakibul.rupom2001@gmail.com>
2025-04-20 06:02:52 +0600

    updated cors link

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://house-rent-client.onrender.com",
+    // origin: "https://house-rent-client.onrender.com",
+    origin: "https://house-finder-rakibul.vercel.app/",
     // origin: "http://localhost:3000",
     credentials: true,
   })
